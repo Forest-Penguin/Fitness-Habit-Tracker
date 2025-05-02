e51cab75e366dd31012ee52d848b01c90a72e47f
Albert Atshemyan <137086173+hackerdon23@users.noreply.github.com>
2025-05-02 15:54:42 -0700

    Create User.java

---
+++ app/src/main/java/com/example/fitness_habit_tracker/database/entities/User.java
@@ -0,0 +1,9 @@
+@Entity(tableName = "users")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    public int userId;
+
+    public String username;
+    public String email;
+    public String password; 
+}
