5f5f43bdb189b0ee6cb445bb33bfdfdbdf143812
Albert Atshemyan <137086173+hackerdon23@users.noreply.github.com>
2025-05-02 22:06:24 -0700

    Delete app/src/main/java/com/example/fitness_habit_tracker/database/dao directory

--- app/src/main/java/com/example/fitness_habit_tracker/database/dao/ProgressDao.java
+++
@@ -1,8 +0,0 @@
-@Dao
-public interface ProgressDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertProgress(Progress progress);
-
-    @Query("SELECT * FROM progress WHERE habitId = :habitId AND date = :date LIMIT 1")
-    Progress getProgressByDate(int habitId, String date);
-}
--- app/src/main/java/com/example/fitness_habit_tracker/database/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-@Dao
-public interface UserDao {
-    @Insert
-    void insertUser(User user);
-
-    @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
-    User getUserByEmail(String email);
-}
